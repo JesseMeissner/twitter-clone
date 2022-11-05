b96cd59eae715f27b9493bfb0ffa501191f046c1
JesseMeissner <jessemeissner3@gmail.com>
2022-11-05 19:23:02 -0400

    Added posting functionality

--- twitter_clone/home/models.py
+++ twitter_clone/home/models.py
@@ -5,13 +5,6 @@ class Post(models.Model):
     class Meta(object):
         db_table = 'post'
 
-    name = models.CharField(
-        'Name', blank=False, null=False, max_length=20, db_index=True, default='Anonymous'
-    )
-    username = models.CharField(
-        'Username', blank=False, null=False, max_length=30, db_index=True, default='Anonymous'
-    )
-
     body = models.CharField(
         'Body', blank=True, null=True, max_length=140, db_index=True
     )
--- twitter_clone/home/views.py
+++ twitter_clone/home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.erros.as_json())
+
     #Get all posts, limit 30
     posts = Post.objects.all()[:30]
 
