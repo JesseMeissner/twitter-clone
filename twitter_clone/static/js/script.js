3429dc17820f321abee952ef00e88ba8aebd1c03
JesseMeissner <jessemeissner3@gmail.com>
2022-11-06 23:37:48 -0500

    Finished styling post form and added upload file button

---
+++ twitter_clone/static/js/script.js
@@ -0,0 +1,6 @@
+const fileBtn = document.getElementById('files')
+const customBtn = document.getElementById('custom_button')
+
+customBtn.addEventListener('click', function() {
+    fileBtn.click()
+});
