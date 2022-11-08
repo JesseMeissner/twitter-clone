c74a407690d06356af0be1e6c2e87c6939bb5f05
JesseMeissner <jessemeissner3@gmail.com>
2022-11-08 01:38:05 -0500

    Redesigned post form to allow for name and username inputs

--- twitter_clone/static/js/script.js
+++ twitter_clone/static/js/script.js
@@ -1,6 +1,7 @@
-const fileBtn = document.getElementById('files')
-const customBtn = document.getElementById('custom_button')
+const fileBtn = document.getElementById('files');
+const customBtn = document.getElementById('custom_button');
+const likesIcon = document.getElementById('likes_icon');
 
 customBtn.addEventListener('click', function() {
-    fileBtn.click()
+    fileBtn.click();
 });
